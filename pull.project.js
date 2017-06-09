eaf87b0e005ebf8f977beb95170df7d2c656250f
Charles. Alacrito <charles.alacrito@stellarloyalty.com>
2017-06-09 15:31:38 +0800

    Added something

--- pull.project.js
+++ pull.project.js
@@ -1,8 +1,11 @@
 
 if(val == "Yes" && val == "y"){
   console.log("hello world");
+  var a = x;
+  if(a == x)
+  {
+    console.log("hooooray!!!!");
+  }
 }else{
   console.log("another comment");
-  
-  
 }
